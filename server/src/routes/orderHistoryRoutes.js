d4728b2ce44bf5a46a9f7c6def3645a3f5bee43a
PhucLa <hohap3@gmail.com>
2026-05-28 20:04:28 +0700

    fix update

--- server/src/routes/orderHistoryRoutes.js
+++ server/src/routes/orderHistoryRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 // Đã sửa chính xác tên file controller viết thường chữ cái đầu
-const orderHistoryController = require("../controllers/orderHistoryController");
+const orderHistoryController = require("../controllers/OrderHistoryController");
 const { verifyToken } = require("../middleware/auth");
 
 router.get("/history", verifyToken, orderHistoryController.getOurOrderHistory);
